281210810fc8c2a2f0b51b688d1ca0396e9a41c1
noorhilmi <47267410+noorhilmi@users.noreply.github.com>
2020-03-14 16:31:10 -0700

    finalizing the code

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -16,63 +16,48 @@ export class AppGroceryComponent {
     tasks = [];
 
     onClick(){
-      
       if(this.task.id == 0){
-        //this.task.name.toLowerCase();
         if (this.itemExists(this.task.name)) {
             alert('This item already exists in your list');
             return;
         }
+
         this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
-      }
-      
+       } 
+       
       this.task = {
         name: '',
         id: 0
       };
-      
     }
+
     itemExists(itemName){
-      
       for(var i = 0;i < this.tasks.length; i++){
-        if(itemName.toLowerCase() == this.tasks[i].name){
+        if(itemName.toLowerCase() === this.tasks[i].name){
           return true;
         }
       }
       return false;
-      
     }
 
     onSortClick(){
-      // this.tasks = this.tasks.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
       this.tasks = this.tasks.sort((a, b) => a.name.localeCompare(b.name))
     }
+
     onEdit(item){
       this.task = item;
     }
 
     onDelete(item){
       for(var i = 0;i < this.tasks.length; i++){
-        if(item.id == this.tasks[i].id){
+        if(item.id === this.tasks[i].id){
           this.tasks.splice(i,1);
           break;
         }
       }
+      this.task = {
+        name: item.name,
+        id: 0
+      };
     }
-
-    // onStrike(item){
-    //   for(var i = 0;i < this.tasks.length; i++){
-    //     if(item.id == this.tasks[i].id){
-    //       if(this.tasks[i].strike){
-    //         this.tasks[i].strike = false;
-    //       }
-    //       else{
-    //         this.tasks[i].strike = true;
-    //       }
-    //       break;
-    //     }
-    //   }
-    // }
-
-  
 }
